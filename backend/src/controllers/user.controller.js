ea98ce6d3335453cbc21b0a9001120976a909c57
Nitin Singh <nitintwt27@gmail.com>
2024-06-10 16:38:44 +0530

    cookie error after deployment

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -89,7 +89,7 @@ const loginUser = asyncHandler(async(req , res)=>{
   
   const options = {
     httpOnly : false,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'None'
   }
 
